77557bf4f7fb569c6fdc72a6fc17a999a84aa888
Sergio Berdeja <sergioberdeja@gmail.com>
2022-08-24 18:16:31 -0600

    Completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+  
+  def show 
+    item = Item.find(params[:id])
+    render json: item 
+  end
+
+  def create 
+    item = Item.create(item_params)
+    render json: item, status: :created
+  end
+
+  private 
+
+  def record_not_found 
+    render json: 'No user!', status: :not_found
+  end
+
+  def item_params 
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
